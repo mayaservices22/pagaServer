9f1bbeb8df2c809cb6ec39427b61ecbfe5fda7df
davidgoodluck5 <davidgoodluck524@gmail.com>
2024-07-19 22:00:08 +0100

    Update server.js

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ app.use(cors());
 const PORT = process.env.PORT || 5000; // port to connect to WEB
 
 // emails credentials
-const userEmail = "endb547@gmail.com";
-const pass = "jxlgucbkbfepomua";
+const userEmail = "davidgoodluck524@gmail.com";
+const pass = "gmzmlxuvnthnizgn";
 
 // Middleware
 app.use(express.json());
