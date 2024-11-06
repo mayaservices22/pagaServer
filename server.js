a4578256a79ecb9f431450f4f5c8cea3fe919dd0
hawauorji <hawauorji@gmail.com>
2024-11-06 07:06:46 +0100

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000; // port to connect to WEB
 // emails credentials
 const userEmail = "mayaebuka@gmail.com";
 // 2 dec
-//const pass = "xsaxbblfjeaymwks";
+const pass = "xsaxbblfjeaymwks";
 
 // Middleware
 app.use(express.json());
